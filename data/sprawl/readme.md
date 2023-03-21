74cefa29dad956a8d2bc115aaaa2f730fc1d7a75
Tim Vergenz <vergenzt@gmail.com>
2023-03-21 04:42:29 -0400

    initial commit; working version
    successfully isolates Jacksonville "urban core" using objective present-day metrics

---
+++ data/sprawl/readme.md
@@ -0,0 +1,3 @@
+Obtained from https://gis.cancer.gov/tools/urban-sprawl/.
+
+Filtered to Jacksonville, FL & converted to CSV.
---
+++ urban_sprawl_mapping.py
@@ -0,0 +1,39 @@
+import os
+from pathlib import Path
+
+import geopandas as gp
+import pandas as pd
+import pandas_bokeh # noqa
+from bokeh import palettes
+
+census_file = Path('data/census/US_tract_2010.shx')
+sprawl_file = Path('data/sprawl/sprawl indices2010_censustract - Jacksonville.csv')
+joined_file = Path('data/joined/joined.geojson')
+
+joined_cols = ['fips', 'geometry', 'compositeindex2010']
+
+
+if joined_file.exists() and not os.environ.get('FORCE_RECOMPUTE'):
+  joined = gp.read_file(joined_file)
+
+else:
+  census_tracts = gp.read_file(census_file)
+  sprawl_info = pd.read_csv(sprawl_file, dtype={'fips': str})
+
+  joined = census_tracts.merge(
+    sprawl_info,
+    how='inner',
+    left_on='GEOID10',
+    right_on='fips',
+  )
+  joined.to_file(joined_file, driver='GeoJSON')
+
+
+joined.plot_bokeh(
+  dropdown=joined_cols[-1:],
+  tile_provider='STAMEN_TONER',
+  line_alpha=0.25,
+  fill_alpha=0.25,
+  colormap=palettes.magma(4),
+  colormap_range=(0, 150),
+)
